702c2dea86c9b12d6ea2c3006cfdac7553d60e9e
Roger Sebastiany <rogersebastiany@gmail.com>
2021-04-02 20:58:26 -0300

    implemented database logic through Docker and Postgres. Finished Cars module. Added Users creation logic.

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,17 +1,23 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at?: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidv4();
-    }
-    if (!this.created_at) {
-      this.created_at = new Date();
     }
   }
 }
--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,23 +1,28 @@
-import { Specification } from "../../models/Specification";
+import { getRepository, Repository } from "typeorm";
+
+import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecificationDTO,
   ISpecificationsRepository,
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
+
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-    Object.assign(specification, { name, description, created_at: new Date() });
-    this.specifications.push(specification);
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
+      description,
+      name,
+    });
+    await this.repository.save(specification);
   }
-  findByName(name: string): Specification {
-    const specification = this.specifications.find(
-      (specification) => specification.name === name
-    );
+  async findByName(name: string): Promise<Specification> {
+    const specification = this.repository.findOne({
+      name,
+    });
     return specification;
   }
 }
