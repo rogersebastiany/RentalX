0bbe878757c076c4035676c8108cc2cb84d00bc4
Roger Sebastiany <rogersebastiany@gmail.com>
2021-04-02 22:02:41 -0300

    User authentication error handling

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -29,13 +30,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or passoword incorrect");
+      throw new AppError("Email or passoword incorrect");
     }
     // Password matches
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("Email or passoword incorrect");
+      throw new AppError("Email or passoword incorrect");
     }
     // Generete JWT
     const token = sign({}, "b4dc17c9461343b39a711ba51a45253c", {
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -19,7 +20,7 @@ class CreateUserUseCase {
   }: ICreateUserDTO): Promise<void> {
     const userAlreadyExists = await this.UsersRepository.findByEmail(email);
     if (userAlreadyExists) {
-      throw new Error("User Already Exists");
+      throw new AppError("User Already Exists");
     }
     const passwordHash = await hash(password, 8);
     await this.UsersRepository.create({
