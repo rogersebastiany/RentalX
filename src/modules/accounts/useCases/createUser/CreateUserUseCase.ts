4875cdee0dccd18f939197c14672d25dee7e2738
Roger Sebastiany <rogersebastiany@gmail.com>
2021-04-02 21:00:01 -0300

    fixed import

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
-import { inject, injectable } from "tsyringe";
 import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
