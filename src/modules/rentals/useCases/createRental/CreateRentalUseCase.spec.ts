7e6e3ccb4c45e79721c853f2110780591d1dcc3a
rogersebastiany <rogersebastiany@gmail.com>
2021-05-10 13:04:48 -0300

    added route test with supertest

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,24 +1,24 @@
 import dayjs from "dayjs";
 
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
-import { DateProvider } from "@shared/container/providers/DateProvider/dayjs/implementations/DateProvider";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
-let dateProvider: DateProvider;
+let dayjsDateProvider: DayjsDateProvider;
 
 describe("Create Rental", () => {
   const dayAdd24Hours = dayjs().add(1, "day").toDate();
 
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
-    dateProvider = new DateProvider();
+    dayjsDateProvider = new DayjsDateProvider();
     createRentalUseCase = new CreateRentalUseCase(
       rentalsRepositoryInMemory,
-      dateProvider
+      dayjsDateProvider
     );
   });
 
